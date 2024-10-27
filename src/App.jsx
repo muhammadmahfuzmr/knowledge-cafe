0a1558816df93f1a9306b842aef709786ed6126d
muhammadmahfuzmr <muhammadmahfuzmr@gmail.com>
2024-10-27 19:04:27 +0600

    added profile picture and fetch json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <>
-      <h1 className="text-5xl bg-red-800">Knowledge Cafe</h1>
-    </>
+    <div className="max-w-[1280px] m-auto mt-3">
+     <Header></Header>
+     <Blogs></Blogs>
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import profile from '../../assets/profile.png'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between align-middle'>
+      <h1 className="text-5xl font-bold">Knowledge Cafe</h1>
+      <img src={profile} alt="" />
+     
+    </div>
+  );
+};
+
+export default Header;
